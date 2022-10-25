267abaef457ab5cd591b43826858cb005e45bd72
tracyneproski <tracyneproski@gmail.com>
2022-10-24 21:49:56 -0500

    Add initial file and scratch file with example

---
+++ mastermind.rb
@@ -0,0 +1,14 @@
+# Build a Mastermind game from the command line where you have 12 turns to guess the secret code, 
+# starting with you guessing the computer’s random code.
+
+class Mastermind
+  
+  def initialize
+  end
+
+
+
+
+
+
+end
---
+++ mastermind_scratch.rb
@@ -0,0 +1,30 @@
+# Notes and scratch stuff
+
+Puzzle contains 4 boxes. Each turn you choose from 6 colors.
+Color choices: R🔴 O🟠 Y🟡 G🟢 B🔵 V🟣
+Example turn: ROGY
+
+Responses:
+● : correct color in correct position
+○ : correct color in incorrect position
+_ : incorrect color
+
+The order of the response tiles does not necessarily match the colored
+characters. 
+Type howto to read these instructions again.
+Type q to quit and show solution.
+
+Turn 1 of 10. Your guess: RGYB
+🔴 🟢 🟡 🔵     ● ○ ○ _
+
+Turn 2 of 10. Your guess: GYBV
+🟢 🟡 🔵 🟣     ● ○ ○ _
+
+Turn 2 of 10. Your guess: q
+You chose to quit. The solution is:
+🟢 🟢 🔴 🟣     ● ○ ○ _
+
+def board_icons
+  
+
+end
